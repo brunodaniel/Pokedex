1f1e91801fa6ee8ff549d7b22ca875d856126f08
Bruno Daniel <bruno.daniel@outlook.com>
2021-10-01 19:03:21 -0300

    update

--- index.js
+++ index.js
@@ -10,8 +10,44 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function(req, res) {
+    const pokedex = [
+        {
+            Numero:001 ,Nome: "Bulbasaur",
+            Tipo:"Grass, Poison",
+            Imagem: "",
+            Descrição: "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
+            Altura: "0.7m",
+            Peso: "6.9kg",
+            Categoria: "seed",
+            Habilidade: "Overgrow"
+        },
     
-    res.render("index", {titulo: "Pokedex"});
+        {
+             Numero:002 ,
+             Nome: "Ivysaur",
+             Tipo:"Grass, Poison",
+             Imagem: "",
+             Descrição: "When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs.",
+             Altura: "1m",
+             Peso: "13kg",
+             Categoria: "",
+             Habilidade: "Overgrow"
+        },
+    
+        {
+             Numero:003,
+             Nome: "Venusaur",
+             Tipo:"Grass, Poison",
+             Imagem: "",
+             Descrição: "Its plant blooms when it is absorbing solar energy. It stays on the move to seek sunlight.",
+             Altura: "2m",
+             Peso: "100kg",
+             Categoria: "",
+             Habilidade: "Overgrow"
+        }
+    ];
+    res.render("index", {titulo: "Pokedex", Nome:pokedex.Nome, Tipo:pokedex.Tipo});
+    
 });
 
 app.get("/cadastro", function(req, res) {
@@ -21,6 +57,11 @@ app.get("/cadastro", function(req, res) {
 app.get("/detalhes", function(req, res) {
     
     res.render("../views/ejs/detalhes", {titulo: "Detalhes"});
+});
+
+app.post("index", (req, res) => {
+    const { name, type } = req.body
+    res.send({ name: Nome, type: Tipo });
 });
 
 // app.post
